b5902ca79d76c7ad5f2012d0ff12395f31bd4e87
snomiao <snomiao@gmail.com>
2022-04-22 17:18:01 +0800

    chores

--- packages/ssr-react/vite.config.mjs
+++ packages/ssr-react/vite.config.mjs
@@ -4,6 +4,21 @@ import { VitePWA } from "vite-plugin-pwa";
 export default defineConfig({
   plugins: [react(), VitePWA({})],
   build: {
-    minify: false,
+    minify: true,
   },
 });
+
+// 1. 天眼查 改动
+//   小区
+//     信息 图片
+//     2手房 的前3个
+//       详情信息全部 和图片
+//       核心之类
+
+// 2. 房产
+//   58
+//     城市 搜索到路
+//     前3个
+//     全爬
+
+// 1.
